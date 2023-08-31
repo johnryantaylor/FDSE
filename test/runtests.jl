835d5979c25818323be4beba01ed22228725787f
Jago Stong-Wright <jagoosw@protonmail.com>
2023-08-31 23:26:03 +0200

    short script to run all projects sequentially

---
+++ test/runtests.jl
@@ -0,0 +1,8 @@
+# Just run all the projects to check they're working
+
+include("../Project1/gravitycurrent.jl")
+include("../Project2/KH.jl")
+include("../Project3/rossbywave.jl")
+include("../Project4/PZ_box.jl")
+include("../Project4/PZ_column.jl")
+include("../Project4/PZ_cavity.jl")
