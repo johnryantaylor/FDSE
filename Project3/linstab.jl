e6f2254489f38b275632d9e65f873689a2ad1324
johnryantaylor <johnryantaylor@gmail.com>
2025-09-08 15:10:41 +0100

    Update linstab.jl
    reduced wavenumber resolution and changed shear layer width to match KH.jl

--- Project3/linstab.jl
+++ Project3/linstab.jl
@@ -5,7 +5,6 @@ using Plotly: attr, Layout
 using Statistics # used in SSF.jl
 using LinearAlgebra  # used in SSF.jl
 
-
 include("./code/SSF.jl")  # We include SSF.jl which contains the function to do the calculation
 # The following functions calculate discrete derivatives
 include("./code/ddz.jl")
@@ -14,8 +13,8 @@ include("./code/ddz4.jl")
 
 # ********** User input parameters **********
 LZ = 1    # The z-domain size
-LX = 10   # The x-domain size (sets the range of wavenumbers to search)
-h = 0.025     # Shear layer width
+LX = 8   # The x-domain size (sets the range of wavenumbers to search)
+h = 0.1     # Shear layer width
 NZ = 100  # The number of gridpoints
 dz = LZ / NZ  # The grid spacing - must be evenly spaced
 nu = 1 / 5000  # Kinematic viscosity (or 1/Re)
@@ -41,13 +40,14 @@ end
 Rig = dbdz ./ (dudz .^ 2)
 
 # Create an x-wavenumber vector to explore solutions
-kx = LinRange(2 * pi / LX, 2 * pi * 200 / LX, 1000)
+kx = LinRange(2 * pi / LX, 2 * pi * 20 / LX, 100)
 
 sigma = complex(zeros(2*(NZ+1), length(kx)))
 lambda_w = complex(zeros(NZ+1, 2*(NZ+1), length(kx)))
 lambda_b = complex(zeros(NZ+1, 2*(NZ+1), length(kx)))
 
 for (k, k_val) in enumerate(kx)
+    println("Working on wavenumber $k / $(length(kx))")
     (sigma[:, k], lambda_w[:, :, k], lambda_b[:, :, k]) = SSF(z, vel, buoy, k_val, 0, nu, kappa, [0, 0], [0, 0], 0)
 end
 
